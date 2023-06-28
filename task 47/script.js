bdd5f5a10d92d8bfedb7bd0060aef6e1c575c22e
Rekunkova Alena <alenarekunkova3017@gmail.com>
2023-06-28 09:45:08 +0300

    new tasks

---
+++ task 47/script.js
@@ -0,0 +1,7 @@
+"use strict"
+let num= parseInt('5px'),num1=parseInt('6px');
+alert(num+num1);
+let a= parseFloat('5.5px'), b=parseFloat('6.25px');
+alert(a+b);
+ let d= parseFloat('5.5px'), c=parseFloat('6.25px');
+alert(d+c +' px');
---
+++ task48/script.js
@@ -0,0 +1,3 @@
+"use strict"
+let str = String(2345), a=String(44241)
+alert(str+a)
